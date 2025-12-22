250f75a2df7d70221fb157de4d404121d8c1befe
lavaaash <vital229008@Mail.ru>
2025-12-23 03:02:19 +0700

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CouseWork3Semester
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
