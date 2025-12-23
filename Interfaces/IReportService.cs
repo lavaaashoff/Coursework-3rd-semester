dae020699b4db575c2244c77f9f65850df30a864
lavaaash <vital229008@Mail.ru>
2025-12-24 02:22:13 +0700

    Added ReportService

---
+++ Interfaces/IReportService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouseWork3Semester.Interfaces
+{
+    public interface IReportService
+    {
+        IDormitoryRegistry DormitoryRegistry { get; }
+        IOccupantRegistry OccupantRegistry { get; }
+        IPaymentService PaymentService { get; }
+
+        string GenerateDormitoryReport();
+        string GenerateSettlementReport(DateTime startDate, DateTime endDate);
+        string GeneratePaymentReport(IRoomOccupant occupant, DateTime period);
+        string GenerateInventoryReport(IDormitory dormitory);
+    }
+}
---
+++ Services/ReportService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouseWork3Semester.Services
+{
+    internal class ReportService
+    {
+    }
+}
