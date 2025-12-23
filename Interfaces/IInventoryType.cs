c07a786df3ade82419bfa9ebb483229b5e5903a0
lavaaash <vital229008@Mail.ru>
2025-12-23 20:52:42 +0700

    Added InventoryType

---
+++ Interfaces/IInventoryType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouseWork3Semester.Interfaces
+{
+    public interface IInventoryType
+    {
+        // Свойства
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Category { get; set; }
+        string Description { get; set; }
+
+        // Методы
+        string GetFullInfo();
+    }
+}
