f3e073949f53043d3466797a10fd2399428d5d7b
lavaaash <vital229008@Mail.ru>
2025-12-23 21:35:10 +0700

    Added IChild

---
+++ Interfaces/IChild.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouseWork3Semester.Interfaces
+{
+    public interface IChild : IRoomOccupant
+    {
+        Guid ParentResidentId { get; }
+    }
+}
