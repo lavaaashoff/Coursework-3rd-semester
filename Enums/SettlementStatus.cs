3dea2257eafe741b9e6d44aa8b7f110cbf5bcdb0
lavaaash <vital229008@Mail.ru>
2025-12-24 00:57:13 +0700

    Added SettlementStatus

---
+++ Enums/SettlementStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouseWork3Semester.Enums
+{
+    public enum SettlementStatus
+    {
+        Initialized,    // Инициализировано
+        InProgress,     // В процессе (выполняется)
+        Completed,      // Завершено
+        Cancelled       // Отменено
+    }
+}
